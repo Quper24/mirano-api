96829db0c9492eeb88714987682458a2609d82da
Maksim Leskin <maksim.leskin@methed.ru>
2024-05-19 00:43:05 +0300

    fix order

--- orderController.js
+++ orderController.js
@@ -1,5 +1,6 @@
 import { randomUUID } from 'crypto';
 import { loadOrders, saveOrders } from './dataService.js';
+import { readCartData, writeCartData } from './cartUtils.js';
 
 export const setupOrderRoutes = app => {
   app.post('/api/orders', async (req, res) => {
@@ -10,34 +11,33 @@ export const setupOrderRoutes = app => {
       paymentOnline,
       deliveryDate,
       deliveryTime,
-      products,
     } = req.body;
 
-    if (!buyer || !recipient || !address || !products) {
+    const accessKey = req.cookies.accessKey;
+    if (!accessKey) return res.status(401).json({ error: 'Access denied' });
+
+    if (!buyer || !recipient) {
       return res
         .status(400)
         .json({ error: 'Missing required information for the order' });
     }
 
-    if (
-      !Array.isArray(products) ||
-      products.some(
-        p => typeof p.id !== 'number' || typeof p.quantity !== 'number',
-      )
-    ) {
-      return res.status(400).json({ error: 'Invalid products list' });
-    }
+    const carts = await readCartData();
+    const cart = carts[accessKey];
+
+    if (!cart?.items?.length)
+      return res.status(404).json({ error: 'Cart not found or empty cart' });
 
     const orders = await loadOrders();
     const order = {
-      id: randomUUID(),
+      id: accessKey,
       buyer,
       recipient,
       address,
       paymentOnline,
       deliveryDate,
       deliveryTime,
-      products,
+      products: cart.items,
     };
 
     orders.push(order);
@@ -45,6 +45,11 @@ export const setupOrderRoutes = app => {
     if (!(await saveOrders(orders))) {
       return res.status(500).json({ error: 'Failed to save the order' });
     }
+
+    const newAccessKey = randomUUID();
+    res.cookie('accessKey', newAccessKey, { httpOnly: true, path: '/' });
+    carts[newAccessKey] = { items: [] };
+    await writeCartData(carts);
 
     res.status(201).json({
       message: 'Order received and saved successfully',
